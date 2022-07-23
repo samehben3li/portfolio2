a2f5ad7bc77596ed3f1e8b85721ba75bf212e336
sameh <samehbenali69@gmail.com>
2022-07-23 20:05:48 +0100

    add blog app to portolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,4 @@
 import "./portfolio.css"
-import IMG1 from "../../assets/portfolio1.jpg"
 import IMG2 from "../../assets/portfolio2.jpg"
 import IMG3 from "../../assets/portfolio3.jpg"
 import IMG4 from "../../assets/portfolio4.jpg"
@@ -14,12 +13,12 @@ const Portfolio = () => {
     <div className="container portfolio__container">
       <article className="portfolio__item">
         <div className="portfolio__item-img">
-          <img src={ IMG1 } alt="" />
+          <img src="/assets/blog.png" alt="" />
         </div>
-        <h3>This is a porfolio item title</h3>
+        <h3>TheKnower Blog .</h3>
         <div className="cta">
-          <a href="https://github.com/" rel="noreferrer" className="btn" target="_blank">Github</a>
-          <a href="https://github.com/" rel="noreferrer" className="btn btn-primary" target="_blank">Live Demo</a>
+          <a href="https://github.com/samehben3li/Blog-App" rel="noreferrer" className="btn" target="_blank">Github</a>
+          <a href="https://theknowerblog.netlify.app/" rel="noreferrer" className="btn btn-primary" target="_blank">Live Demo</a>
         </div>
       </article>
       <article className="portfolio__item">
