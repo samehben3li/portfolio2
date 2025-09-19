2bb0ee72455dd8fabc1d2043d2a0f02661d9524e
sameh11 <sameh@ranadev.io>
2025-09-19 01:34:05 +0100

    chore: update email

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,24 +1,32 @@
-import "./contact.css"
-import { MdOutlineEmail } from "react-icons/md"
-import { RiMessengerLine } from "react-icons/ri"
-import { BsWhatsapp } from "react-icons/bs"
-import emailjs from "emailjs-com"
-import { useRef } from "react"
+import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
+import emailjs from "emailjs-com";
+import { useRef } from "react";
 
 const Contact = () => {
-
-  const form = useRef()
+  const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_byy3j5m', 'template_wr5ro09', form.current, 'GBu0JBt40KfsB2xcD')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        "service_byy3j5m",
+        "template_wr5ro09",
+        form.current,
+        "GBu0JBt40KfsB2xcD"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
-      e.target.reset()
+        }
+      );
+    e.target.reset();
   };
 
   return (
@@ -28,33 +36,59 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineEmail className="contact__option-icon"/>
+            <MdOutlineEmail className="contact__option-icon" />
             <h4>Email</h4>
-            <h5>samehbenali415@gmail.com</h5>
-            <a href="mailto:samehbenali415@gmail.com" target="_blank" rel="noreferrer">Send a message</a>
+            <h5>sameh@ranadev.io</h5>
+            <a href="mailto:sameh@ranadev.io" target="_blank" rel="noreferrer">
+              Send a message
+            </a>
           </article>
           <article className="contact__option">
-            <RiMessengerLine className="contact__option-icon"/>
+            <RiMessengerLine className="contact__option-icon" />
             <h4>Messenger</h4>
             <h5>sameh.ben3ali</h5>
-            <a href="https://m.me/sameh.ben3ali" target="_blank" rel="noreferrer">Send a message</a>
+            <a
+              href="https://m.me/sameh.ben3ali"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
           </article>
           <article className="contact__option">
-            <BsWhatsapp className="contact__option-icon"/>
+            <BsWhatsapp className="contact__option-icon" />
             <h4>WhatsApp</h4>
             <h5>+216 20 401 295</h5>
-            <a href="https://api.whatsapp.com/send?phone=+21620401295" target="_blank" rel="noreferrer">Send a message</a>
+            <a
+              href="https://api.whatsapp.com/send?phone=+21620401295"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
           </article>
         </div>
         <form ref={form} onSubmit={sendEmail}>
-          <input type="text" name="name" placeholder="Your Full Name" required/>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
           <input type="email" name="email" placeholder="Your Email" required />
-          <textarea name="message" rows="7" placeholder="Your Message" required></textarea>
-          <button type="submit" className="btn btn-primary">Send Message</button>
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
         </form>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
